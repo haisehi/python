c101cfb4e0fdf0207fce209c43256d9d8bdefd0d
haisehi <88788629+haisehi@users.noreply.github.com>
2024-05-24 21:11:38 +0700

    build project SVM

--- a.py
+++ a.py
@@ -1,5 +1,5 @@
 from PIL import Image
-img = Image.open(r"C:\Users\Quang Huy\Downloads\ML\New folder\3040.png")
-angle = 20
+img = Image.open(r"D:\code\python\project_phan_loai_ban_tay\train1-dataset-image\2786.png")
+angle = 328
 rotate_img= img.rotate(angle, expand = True)
 rotate_img.show()
